5c4555895e896a439c4adc36bde2f4c72eb93ab2
Abdullah-Sonija <abdullahsonija28@gmail.com>
2024-10-03 19:37:59 +0500

    Update digit_sum.c

--- LAB Assignments/LAB5/digit_sum.c
+++ LAB Assignments/LAB5/digit_sum.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 int main()
 {
-	int n, last_digit, a, sum = 0;
+	int n, last_digit, a, sum;
 	printf("Enter the Number: ");
 	scanf("%d", &n);
 	a = n;
-	while(n > 0)
+	while(a >= 10)
 	{
-		last_digit = n % 10;
-		sum = sum + last_digit;
-		n = n / 10;
+		sum = 0;
+		while(a > 0)
+		{
+			last_digit = a % 10;
+			sum = sum + last_digit;
+			a = a / 10;
+		}
+		a = sum;
 	}
-	printf("The sum of digits of the %d is = %d", a, sum);
+    printf("The sum of digits of %d till the answer is a single digit number is = %d\n", n, a);
+
 	return 0;
 }
