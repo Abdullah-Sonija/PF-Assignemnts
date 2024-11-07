f20a2b9c245218763ccd49ed4b3b3aec7ca438f1
Abdullah-Sonija <abdullahsonija28@gmail.com>
2024-11-07 19:59:22 +0500

    Update q3.c

--- LAB Assignments/Assignment 2/Question 3/q3.c
+++ LAB Assignments/Assignment 2/Question 3/q3.c
@@ -1,60 +1,58 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
-//function to eliminate the same character
-void compress1(const char* word,char* new_word,int* words_removed)
+// Function to eliminate consecutive duplicate characters
+void compress1(const char* word, char* new_word, int* words_removed) 
 {
-    int i,j=0;
-    *words_removed=0;
+    int i, j = 0;
+    *words_removed = 0;
     
-    for(i=0;word[i]!='\0';i++)
+    for (i = 0; word[i] != '\0'; i++) 
     {
-        if(i==0||word[i]!=word[i-1])
+        if (i == 0 || word[i] != word[i - 1]) 
         {
-            new_word[j++]=word[i];
-        }
-        else
+            new_word[j++] = word[i];
+        } 
+        else 
         {
             (*words_removed)++;
         }
     }
-    new_word[j]='\0';
+    new_word[j] = '\0';
 }
 
-//function to eliminate multiple words
-void compress2(const char* words[],int n)
+// Function to eliminate duplicates in multiple words
+void compress2(const char words[][40], int n) 
 {
     char new_word[100];
-    int total_removed=0;
-    for(int i=0;i<n;i++)
+    int total_removed = 0;
+    
+    for (int i = 0; i < n; i++) 
     {
-        int words_removed=0;
+        int words_removed = 0;
         compress1(words[i], new_word, &words_removed);
-        total_removed+=words_removed;
+        total_removed += words_removed;
         printf("\nOriginal: \"%s\".\nCorrected word: \"%s\".\nNo of Extra Characters Removed: %d\n", words[i], new_word, words_removed);
     }
     printf("\nTotal Characters Removed: %d\n", total_removed);
 }
 
-int main()
+int main() 
 {
-    int n, i;
-    printf("How many words you want to enter: ");
+    int n;
+    printf("How many words do you want to enter? ");
     scanf("%d", &n);
-    //Taking input
-        char** words = (char**)malloc(n * sizeof(char*));
-    for(i=0;i<n;i++)
+
+    char words[n][40];
+    
+    // Taking input
+    for (int i = 0; i < n; i++) 
     {
-        words[i] = (char*)malloc(40 * sizeof(char));
-        printf("Enter the word %d: ", i+1);
+        printf("Enter word %d: ", i + 1);
         scanf("%39s", words[i]);
     }
-    compress2((const char**)words, n);
-    for (i = 0; i < n; i++)
-    {
-        free(words[i]);
-    }
-    free(words);
+
+    compress2(words, n);
+
     return 0;
 }
